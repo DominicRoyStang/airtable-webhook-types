a83f529cdd14ca9041126e4f05e92a7c167382bc
Dominic Roy-Stang <dominicroystang@gmail.com>
2024-09-10 15:07:07 -0700

    complete webhookTableChangedSchema

--- index.ts
+++ index.ts
@@ -44,11 +44,60 @@ export const webhookActionSchema = z.discriminatedUnion('source', [
 ])
 export type WebhookAction = z.infer<typeof webhookActionSchema>
 
+export const webhookRecordChangedSchema = z.object({
+  current: z.object({
+    cellValuesByFieldId: z.record(cellsSchema.nullable()),
+  }),
+  previous: z.object({
+    cellValuesByFieldId: z.record(cellsSchema.nullable()),
+  }).optional(),
+  unchanged: z.object({
+    cellValuesByFieldId: z.record(cellsSchema.nullable()),
+  }).optional(),
+})
+export type WebhookRecordChanged = z.infer<typeof webhookRecordChangedSchema>
+
+export const webhookRecordCreatedSchema = z.object({
+  timestamp: z.string().datetime(),
+  cellValuesByFieldId: z.record(cellsSchema),
+})
+export type WebhookRecordCreated = z.infer<typeof webhookRecordCreatedSchema>
+
 // As per documentation: https://airtable.com/developers/web/api/model/webhooks-table-changed
 export const webhookTableChangedSchema = z.object({
-  changedViewsById: z.object({}).optional(), // TODO
-  changedFieldsById: z.object({}).optional(), // TODO
-  changedRecordsById: z.object({}).optional(), // TODO
+  changedViewsById: z.record(z.object({
+    changedRecordsById: z.record(webhookRecordChangedSchema).optional(),
+    createdRecordsById: z.record(webhookRecordCreatedSchema).optional(),
+    destroyedRecordIds: z.string().array().optional(),
+  })).optional(),
+  changedFieldsById: z.record(z.object({
+    current: z.object({
+      type: fieldTypeSchema.optional(),
+      name: z.string().optional(),
+    }),
+    previous: z.object({
+      type: fieldTypeSchema.optional(),
+      name: z.string().optional(),
+    }).optional(),
+  })).optional(),
+  changedRecordsById: z.record(webhookRecordChangedSchema).optional(),
+  createdFieldsById: z.record(z.object({
+    type: fieldTypeSchema,
+    name: z.string(),
+  })).optional(),
+  createdRecordsById: z.record(webhookRecordCreatedSchema).optional(),
+  changedMetadata: z.object({
+    current: z.object({
+      name: z.string().optional(),
+      description: z.string().nullable().optional(),
+    }),
+    previous: z.object({
+      name: z.string().optional(),
+      description: z.string().nullable().optional(),
+    }),
+  }).optional(),
+  destroyedFieldIds: z.string().array().optional(),
+  destroyedRecordIds: z.string().array().optional(),
 })
 export type WebhookTableChanged = z.infer<typeof webhookTableChangedSchema>
 
